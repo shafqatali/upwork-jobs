274d00305284b20c1bad8064a7a3770eec997924
Shafqat Ali <shafqat@kco.ie>
2020-01-13 14:31:22 +0500

    Add a window application to parse table from HTML5 file.

---
+++ HtmlParse/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace HtmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.fbdOutput = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Input File";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(163, 13);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(890, 26);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.Click += new System.EventHandler(this.txtFilePath_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(1059, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(129, 29);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse &File";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(163, 86);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(159, 29);
+            this.btnParse.TabIndex = 3;
+            this.btnParse.Text = "&Parse HTML File";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.SupportMultiDottedExtensions = true;
+            this.ofdFile.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdFile_FileOk);
+            // 
+            // dgView
+            // 
+            this.dgView.AllowUserToAddRows = false;
+            this.dgView.AllowUserToDeleteRows = false;
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgView.Location = new System.Drawing.Point(30, 147);
+            this.dgView.Name = "dgView";
+            this.dgView.ReadOnly = true;
+            this.dgView.Size = new System.Drawing.Size(1140, 528);
+            this.dgView.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output Folder";
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(163, 54);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.ReadOnly = true;
+            this.txtOutputFolder.Size = new System.Drawing.Size(890, 26);
+            this.txtOutputFolder.TabIndex = 6;
+            this.txtOutputFolder.Click += new System.EventHandler(this.txtOutputFolder_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1059, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Br&owse Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Extracted Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 705);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgView);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(30);
+            this.Text = "Html Parser";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.DataGridView dgView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.FolderBrowserDialog fbdOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HtmlParse/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Threading;
+using Newtonsoft.Json;
+
+using DataTable = System.Data.DataTable;
+using System.Diagnostics;
+
+namespace HtmlParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ofdFile.ShowDialog();
+        }
+
+        private void txtFilePath_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilePath.Text))
+            {
+                ofdFile.ShowDialog();
+            }
+        }
+
+        private void txtOutputFolder_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOutputFolder.Text))
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdOutput.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                txtOutputFolder.Text = fbdOutput.SelectedPath;
+            }
+        }
+
+        private void ofdFile_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ofdFile.FileName))
+            {
+                txtFilePath.Text = ofdFile.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ofdFile.FileName = "";
+            ofdFile.Filter = "html files (*.htm)|*.htm|HTML files (*.html)|*.html|All files (*.*)|*.*";
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtFilePath.Text) && string.IsNullOrEmpty(txtOutputFolder.Text))
+            {
+                MessageBox.Show("Please select input file and output folder first.", "Infomation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+
+            }
+            btnParse.Enabled = false;
+            btnParse.Text = " Parsing HTML file ....";
+            string fileToParse = txtFilePath.Text;
+            if (File.Exists(fileToParse))
+            {
+                var fileparts = fileToParse.Split('\\');
+                var realName = fileparts[fileparts.Length - 1];
+
+                List<Data> list = Parse_HTML_File(fileToParse);
+                if(list.Count > 0)
+                {
+                    dgView.AutoGenerateColumns = true;
+                    dgView.DataSource = list;
+
+                    for (int c = 0; c < dgView.Columns.Count; c++)
+                    {
+                        dgView.Columns[c].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    }
+
+                    string fileName = WriteCSV(list, realName);
+
+                    DialogResult dr = MessageBox.Show("HTML File parsed successfully. Open Excel File?", "Output", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (dr == DialogResult.Yes)
+                    {
+                        Process.Start("explorer.exe", fileName);
+                    }
+                }else
+                {
+                    MessageBox.Show("The input file doesn't have required data.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The file doesn't exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btnParse.Enabled = true;
+            btnParse.Text = "&Parse HTML File";
+        }
+
+        private List<Data> Parse_HTML_File(string fileName)
+        {
+            List<Data> listData = new List<Data>();
+            try
+            {
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.Load(fileName);
+
+
+                var tableMarkup = doc.DocumentNode.SelectSingleNode("//table");
+
+                HtmlNodeCollection tableRows = tableMarkup.ChildNodes;
+
+                foreach (var row in tableRows)
+                {
+                    if (row.NodeType == HtmlNodeType.Element)
+                    {
+                        HtmlNodeCollection tableCols = row.ChildNodes;
+                        Data d = new Data();
+                        var count = tableCols.Count;
+                        if (count >= 4)
+                        {
+                            int counter = 1;
+                            foreach (var col in tableCols)
+                            {
+
+                                if (col.NodeType == HtmlNodeType.Element && col.Name == "td" && counter < 4)
+                                {
+                                    var column = col;
+                                    if (counter == 1)
+                                    {
+                                        string colText = col.InnerText;
+                                        try
+                                        {
+                                            int num = Convert.ToInt32(colText);
+                                            d.Id = num + 1;
+                                        }
+                                        catch (Exception)
+                                        {
+                                            d.Id = 0;
+                                        }
+                                    }
+                                    if (counter == 2)
+                                    {
+                                        d.Name = col.InnerHtml;
+                                    }
+                                    if (counter == 3)
+                                    {
+                                        d.Uid = col.InnerHtml;
+                                    }
+                                }
+                                counter += 1;
+                            }
+                        }
+                        if (d.Id != 0 && !string.IsNullOrEmpty(d.Name) && !string.IsNullOrEmpty(d.Uid))
+                        {
+                            listData.Add(d);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to parse data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return listData;
+        }
+
+        private string WriteCSV(List<Data> list, string realName)
+        {
+            var json = JsonConvert.SerializeObject(list);
+                string fileName = txtOutputFolder.Text + @"\"+ DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + "-"+ realName +".csv";
+
+                DataTable dataTable = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+
+                var lines = new List<string>();
+
+                string[] columnNames = dataTable.Columns
+                    .Cast<DataColumn>()
+                    .Select(column => column.ColumnName)
+                    .ToArray();
+
+                var header = string.Join(",", columnNames.Select(name => $"\"{name}\""));
+                lines.Add(header);
+
+                var valueLines = dataTable.AsEnumerable()
+                    .Select(row => string.Join(",", row.ItemArray.Select(val => $"\"{val}\"")));
+
+                lines.AddRange(valueLines);
+
+                File.WriteAllLines(fileName, lines);
+
+            return fileName;
+        }
+
+        
+    }
+
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Uid { get; set; }
+    }
+}
+
